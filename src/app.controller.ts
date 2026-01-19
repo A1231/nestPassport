351c1e545a770c09d204c8dfb1be166169f6a360
A1231 <37673364+A1231@users.noreply.github.com>
2026-01-19 16:51:20 -0500

    ENable JWTAuthGuard globally

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,20 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 
-import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
